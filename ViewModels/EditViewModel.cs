72d655ad215b1dedf797d550e0e7a4161e6dad91
Kubilay Birer <kubilaybirer@hotmail.com>
2024-08-02 14:18:15 +0300

    created user edit form with password reset and get,post functions

---
+++ ViewModels/EditViewModel.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace IdentityApp.ViewModels
+{
+    public class EditViewModel
+    {
+        public string? Id { get; set; }
+        public string? FullName {get;set;} 
+
+         [EmailAddress]
+        public string? Email {get;set;} 
+
+        [DataType(DataType.Password)]
+        public string? Password {get;set;} 
+
+        [DataType(DataType.Password)]
+        [Compare("Password",ErrorMessage ="Passwords don't match.")]
+        public string? ConfirmPassword {get;set;} 
+
+
+    }
+}
--- obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9feff770e77b6b816e01b0954e513aaf453d0fee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea671243270927ffe8f97bd4119339c5871a68fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
