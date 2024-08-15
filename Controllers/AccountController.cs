2959cc933d380c316da827c367757ad8aae90d95
Kubilay Birer <kubilaybirer@hotmail.com>
2024-08-15 12:41:08 +0300

    Login authentication completed with sign in manager and cookies

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,67 @@
+using IdentityApp.Models;
+using IdentityApp.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityApp.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<AppUser> _userManager;
+        private RoleManager<AppRole> _roleManager;
+        private SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> userManager,RoleManager<AppRole> roleManager,SignInManager<AppUser> signInManager)
+        {
+            _userManager=userManager;
+            _roleManager=roleManager;
+            _signInManager=signInManager;
+        }
+
         public IActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if(ModelState.IsValid)
+            {
+                var user = await _userManager.FindByEmailAsync(model.Email);
+
+                if(user!=null)
+                {
+                    await _signInManager.SignOutAsync();
+
+                    var result = await _signInManager.PasswordSignInAsync(user,model.Password,model.RememberMe,true);
+
+                    if(result.Succeeded)
+                    {
+                        await _userManager.ResetAccessFailedCountAsync(user);
+                        await _userManager.SetLockoutEndDateAsync(user,null);
+
+                        return RedirectToAction("Index","Home");
+                    }
+                    else if(result.IsLockedOut)
+                    {
+                        var lockoutDate = await _userManager.GetLockoutEndDateAsync(user);
+                        var timeleft=lockoutDate.Value - DateTime.UtcNow;
+
+                        ModelState.AddModelError("", $"Your Account is locked for     {timeleft.Minutes} minutes.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("","Your password is wrong");
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("","Not found an account with typed email adress");
+                }
+            }
+
+            return View(model);
         }
     }
 }
--- obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/IdentityApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af0401ec37ebdda1bcaa3b22a4a4b956a54d54b9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a7c7057766414fc331c74cdd41d7a067a167ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
